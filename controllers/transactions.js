8c697ab5e2c1332b66040e45dafa6ee30645297b
Fred Moore <fmoore1124@gmail.com>
2020-10-18 11:40:44 -0400

    set up routes and transaction controller

---
+++ controllers/transactions.js
@@ -0,0 +1,20 @@
+// @desc      Get all transactions
+// @route     GET /api/v1/transactions
+//@access     Public
+exports.getTransactions = (req, res, next) => {
+  res.send('Get transactions')
+}
+
+// @desc      Add transaction
+// @route     POST /api/v1/transactions
+//@access     Public
+exports.addTransactions = (req, res, next) => {
+  res.send('Post transaction')
+}
+
+// @desc      Delete transaction
+// @route     DELETE /api/v1/transactions/:id
+//@access     Public
+exports.deleteTransactions = (req, res, next) => {
+  res.send('Delete transaction')
+}
