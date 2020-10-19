1150e376cfee8633ab7afefc2c168fd4e338f684
Fred Moore <fmoore1124@gmail.com>
2020-10-19 07:54:02 -0400

    added backend to frontend

--- client/src/components/TransactionList.js
+++ client/src/components/TransactionList.js
@@ -1,16 +1,23 @@
-import React, { useContext } from 'react';
-import { Transaction } from './Transaction';
+import React, { useContext, useEffect } from 'react'
+import { Transaction } from './Transaction'
 
-import { GlobalContext } from '../context/GlobalState';
+import { GlobalContext } from '../context/GlobalState'
 
 export const TransactionList = () => {
-  const { transactions } = useContext(GlobalContext);
+  const { transactions, getTransactions } = useContext(GlobalContext)
+
+  useEffect(() => {
+    getTransactions()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <>
       <h3>History</h3>
-      <ul className="list">
-        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
+      <ul className='list'>
+        {transactions.map((transaction) => (
+          <Transaction key={transaction._id} transaction={transaction} />
+        ))}
       </ul>
     </>
   )
